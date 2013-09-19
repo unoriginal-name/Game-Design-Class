c3e26f78783c1c2a7d3de73796158a3202cf3d86
Devin Schwab <digidevin@gmail.com>
2013-09-19 22:38:38 +0800

    Have a main menu with 3 buttons. Only the first button works. But when clicked it launches Pat's demo and then when escape (Android back) is pressed it goes back to the main menu

--- quarantine/Assets/Scripts/MainMenu.cs
+++ quarantine/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,23 @@ public class MainMenu : MonoBehaviour {
 	
 	}
 	
+	void OnGUI() {
+		if(GUI.Button (new Rect(20, 20, Screen.width-40, (Screen.height-40)/3), "Pat's Demo"))
+		{
+			Application.LoadLevel ("TestScene");	
+		}
+		
+		if(GUI.Button (new Rect(20, 20 + (Screen.height-40)/3, Screen.width-40, (Screen.height-40)/3), "Rachid's Demo"))
+		{
+			// load Rachid's demo here
+		}
+		
+		if(GUI.Button (new Rect(20, 20+(Screen.height-40)*2/3, Screen.width-40, (Screen.height-40)/3), "Devin's Demo"))
+		{
+			// load Devin's demo here
+		}
+	}
+	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space))
