78e76922098a5ca7979c7f69cc6c4bfdd5ac5dda
Devin Schwab <digidevin@gmail.com>
2013-09-27 10:52:54 +0800

    Added a TestScene with the WWWTest script. Also added a MiniJSON library.
    Currently have an example to serialize and deserialize JSON

---
+++ Newtonsoft.Json.Net/Newtonsoft.Json.Net/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Newtonsoft.Json.Net
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ quarantine/Assets/Scripts/WWWTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class WWWTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var jsonString = "{\"array\": [1.44, 2, 3], " +
+							"\"object\": {\"key1\":\"value1\", \"key2\":256}, " +
+							"\"string\": \"The quick brown fox \\\"jumps\\\" over the lazy dog \", " +
+							"\"unicode\": \"\\u3041 Men\u00fa sesi\u00f3n\", " +
+							"\"int\": 65536, " +
+							"\"float\": 3.14159, " +
+							"\"bool\": true, " +
+							"\"null\": null }";
+		
+		var dict = Json.Deserialize(jsonString) as Dictionary<string,object>;
+		
+		Debug.Log ("deserialized: " + dict.GetType());
+		Debug.Log ("dict['array'][0]: " + ((List<object>) dict["array"])[0]);
+		Debug.Log ("dict['string']: " + (string) dict["string"]);
+		Debug.Log ("dict['float']: " + (double)dict["float"]);
+		Debug.Log ("dict['int']: " + (long) dict["int"]);
+		Debug.Log ("dict['unicode']: " + (string) dict["unicode"]);
+		
+		var str = Json.Serialize(dict);
+		
+		Debug.Log ("serialized: " + str);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
