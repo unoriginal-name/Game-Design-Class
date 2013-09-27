1b13f23598662fd1811471eb377a93f5e784ee84
Devin Schwab <digidevin@gmail.com>
2013-09-27 15:40:47 +0800

    Now displays a registration page when a user opens the app for the first time.

--- quarantine/Assets/Scripts/MainMenu.cs
+++ quarantine/Assets/Scripts/MainMenu.cs
@@ -31,6 +31,14 @@ public class MainMenu : MonoBehaviour {
 	public Texture btmBarTexture;
 	private Rect btmBarRect;
 	
+	public GUIStyle regularBtnStyle;
+	
+	private Rect registerBtnRect;
+	private Rect entryRect;
+	private Rect instrRect;
+	
+	private string entryText = "username";
+	
 	// Use this for initialization
 	void Start () {
 		horizRatio = GlobalScreenResolution.SharedInstance.widthRatio;
@@ -58,36 +66,69 @@ public class MainMenu : MonoBehaviour {
 		inventoryBtnRect = new Rect(creatureBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
 		playersBtnRect = new Rect(inventoryBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
 		attacksBtnRect = new Rect(playersBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
+		
+		Vector2 labelSizes = new Vector2(350, 50);
+		instrRect = new Rect((Screen.width - labelSizes.x*horizRatio)/2,
+			(Screen.height - labelSizes.y*vertRatio)/2 - labelSizes.y*vertRatio,
+			labelSizes.x*horizRatio,
+			labelSizes.y*vertRatio);
+		
+		Vector2 entrySizes = new Vector2(300, 45);
+		entryRect = new Rect((Screen.width - entrySizes.x*horizRatio)/2,
+			(Screen.height - entrySizes.y*vertRatio)/2,
+			entrySizes.x*horizRatio,
+			entrySizes.y*vertRatio);
+		
+		Vector2 registerBtnSizes = new Vector2(300, 60);
+		registerBtnRect = new Rect((Screen.width - registerBtnSizes.x*horizRatio)/2,
+			(Screen.height - registerBtnSizes.y*vertRatio)/2 + registerBtnSizes.y*vertRatio, 
+			registerBtnSizes.x*horizRatio, 
+			registerBtnSizes.y*vertRatio);
 	}
 	
 	void OnGUI() {
 		GUI.DrawTexture(topBarRect, topBarTexture);
 		GUI.DrawTexture(btmBarRect, btmBarTexture);
 		
-		if(GUI.Button (friendButtonRect, "", friendButtonStyle))
+		// if the user doesn't exist
+		// only display the user creation gui
+		if(!PlayerPrefs.HasKey ("username"))
 		{
-			Debug.Log ("friends button pressed");
+			GUI.Label(instrRect, "Please enter your username");
+			entryText = GUI.TextField(entryRect, entryText);
+			if(GUI.Button (registerBtnRect, "Register", regularBtnStyle))
+			{
+				Debug.Log ("register button pressed: " + entryText);
+			}
+			
+			
+		} else {
+			if(GUI.Button (friendButtonRect, "", friendButtonStyle))
+			{
+				Debug.Log ("friends button pressed");
+			}
+			if(GUI.Button (messagesButtonRect, "", messagesButtonStyle))
+			{
+				Debug.Log ("messages button pressed");
+			}
+			if(GUI.Button (creatureBtnRect, "", creatureBtnStyle))
+			{
+				Debug.Log ("creature button pressed");
+			}
+			if(GUI.Button (inventoryBtnRect, "", inventoryBtnStyle))
+			{
+				Debug.Log ("inventory button pressed");
+			}
+			if(GUI.Button (playersBtnRect, "", playersBtnStyle))
+			{
+				Debug.Log ("players button pressed");
+			}
+			if(GUI.Button (attacksBtnRect, "", attacksBtnStyle))
+			{
+				Debug.Log ("attacks button pressed");
+			}
 		}
-		if(GUI.Button (messagesButtonRect, "", messagesButtonStyle))
-		{
-			Debug.Log ("messages button pressed");
-		}
-		if(GUI.Button (creatureBtnRect, "", creatureBtnStyle))
-		{
-			Debug.Log ("creature button pressed");
-		}
-		if(GUI.Button (inventoryBtnRect, "", inventoryBtnStyle))
-		{
-			Debug.Log ("inventory button pressed");
-		}
-		if(GUI.Button (playersBtnRect, "", playersBtnStyle))
-		{
-			Debug.Log ("players button pressed");
-		}
-		if(GUI.Button (attacksBtnRect, "", attacksBtnStyle))
-		{
-			Debug.Log ("attacks button pressed");
-		}
+		
 	}
 	
 	// Update is called once per frame
