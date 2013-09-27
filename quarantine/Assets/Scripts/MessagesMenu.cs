ef55468474eed3f00b31f019ff38c8a58bf81b18
Devin Schwab <digidevin@gmail.com>
2013-09-27 20:52:54 +0800

    Message receiving and sending is working. Although it isn't pretty.

--- quarantine/Assets/Scripts/MessagesMenu.cs
+++ quarantine/Assets/Scripts/MessagesMenu.cs
@@ -5,12 +5,28 @@ using System.Text;
 
 public class MessagesMenu : MonoBehaviour {
 	
+	float horizRatio;
+	float vertRatio;
+
 	public MainMenu main_menu;
 	
 	private StringBuilder sb;
 	
+	private Rect toRect;
+	private Rect messageRect;
+	private Rect sendRect;
+	
+	private string toField = "To";
+	private string content = "Message";
+	
 	// Use this for initialization
 	void Start () {
+		horizRatio = GlobalScreenResolution.SharedInstance.widthRatio;
+		vertRatio = GlobalScreenResolution.SharedInstance.heightRatio;
+			
+		toRect = new Rect(20*horizRatio, Screen.height/2, (Screen.width)/2 - 40*horizRatio, 60*vertRatio);
+		messageRect = new Rect(20*horizRatio, toRect.yMax + 20*vertRatio, Screen.width - 40*horizRatio, 150*vertRatio);
+		sendRect = new Rect(20*horizRatio, messageRect.yMax + 20 * vertRatio, Screen.width/2 - 40*horizRatio, 70*vertRatio);
 	
 	}
 	
@@ -43,5 +59,15 @@ public class MessagesMenu : MonoBehaviour {
 	// Update is called once per frame
 	void OnGUI () {
 		GUI.Label (main_menu.contextMenuRect, sb.ToString(), main_menu.centeredTextStyle);
+		toField = GUI.TextField(toRect, toField);
+		content = GUI.TextArea(messageRect, content);
+		if(GUI.Button(sendRect, "Send", main_menu.regularBtnStyle))
+		{
+			Debug.Log ("Send button pressed");
+			var webserver = new Webserver("http://immunitygame390.appspot.com");
+			webserver.sendMessage(toField, PlayerPrefs.GetString("username"), content);
+			toField = "";
+			content = "";
+		}
 	}
 }
