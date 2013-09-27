6782c7899ed79566265076a28078d80a31ad53f7
Devin Schwab <digidevin@gmail.com>
2013-09-27 11:29:53 +0800

    Added a friends button to the main menu

--- quarantine/Assets/Scripts/MainMenu.cs
+++ quarantine/Assets/Scripts/MainMenu.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+	
+	public GUIStyle friendButtonStyle;
+	private Rect friendButtonRect;
+	public GUIStyle messagesButtonStyle;
+	public GUIStyle regularButtonStyle;
+	
 	// Use this for initialization
 	void Start () {
-	
+		friendButtonRect =  new Rect(20,20, 1000*friendButtonStyle.normal.background.texelSize.x + 20, 1000*friendButtonStyle.normal.background.texelSize.y + 20);
 	}
 	
 	void OnGUI() {
+		/*
 		if(GUI.Button (new Rect(20, 20, Screen.width-40, (Screen.height-40)/3), "Pat's Demo"))
 		{
 			Application.LoadLevel ("TestScene");	
@@ -23,6 +29,10 @@ public class MainMenu : MonoBehaviour {
 		{
 			// load Devin's demo here
 			Application.LoadLevel("WebRequester");
+		}*/
+		if(GUI.Button (friendButtonRect, "", friendButtonStyle))
+		{
+			Debug.Log ("friends button pressed");
 		}
 	}
 	
