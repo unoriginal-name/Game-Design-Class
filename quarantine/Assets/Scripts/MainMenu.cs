dffd02e5e76f56acc757852c1d20fb7f6a5dff5c
Devin Schwab <digidevin@gmail.com>
2013-09-27 14:24:02 +0800

    Main menu layout with placeholder graphics is working. Just need to implement the button functionalities

--- quarantine/Assets/Scripts/MainMenu.cs
+++ quarantine/Assets/Scripts/MainMenu.cs
@@ -4,19 +4,31 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 	private float horizRatio;
 	private float vertRatio;
-	
-	public GUIStyle friendButtonStyle;
-	public GUIStyle messagesButtonStyle;
-	public GUIStyle regularButtonStyle;
-	
+		
 	public Vector2 top_button_sizes;
 	public Vector2 top_button_offsets;
+	
+	public GUIStyle friendButtonStyle;
 	private Rect friendButtonRect;
+	public GUIStyle messagesButtonStyle;
 	private Rect messagesButtonRect;
 	
 	public Texture topBarTexture;
-	public Texture btmBarTexture;
 	private Rect topBarRect;
+	
+	public Vector2 btmBtnSizes;
+	public Vector2 btmBtnOffsets;
+	
+	public GUIStyle creatureBtnStyle;
+	private Rect creatureBtnRect;
+	public GUIStyle inventoryBtnStyle;
+	private Rect inventoryBtnRect;
+	public GUIStyle playersBtnStyle;
+	private Rect playersBtnRect;
+	public GUIStyle attacksBtnStyle;
+	private Rect attacksBtnRect;
+	
+	public Texture btmBarTexture;
 	private Rect btmBarRect;
 	
 	// Use this for initialization
@@ -27,6 +39,8 @@ public class MainMenu : MonoBehaviour {
 		Debug.Log ("horizRatio: " + horizRatio);
 		Debug.Log ("vertRatio: " + vertRatio);
 		
+		topBarRect = new Rect(0, 0, Screen.width, (2*top_button_offsets.y + top_button_sizes.y)*vertRatio);
+
 		friendButtonRect = new Rect(top_button_offsets.x*horizRatio, 
 			top_button_offsets.y*vertRatio, 
 			top_button_sizes.x*horizRatio, 
@@ -37,11 +51,18 @@ public class MainMenu : MonoBehaviour {
 			top_button_sizes.x*horizRatio, 
 			top_button_sizes.y*vertRatio);
 		
-		topBarRect = new Rect(0, 0, Screen.width, (2*top_button_offsets.y + top_button_sizes.y)*vertRatio);
+		btmBarRect = new Rect(0, Screen.height-(2*btmBtnOffsets.y + btmBtnSizes.y)*vertRatio, Screen.width, (2*btmBtnOffsets.y + btmBtnSizes.y)*vertRatio);
+		
+		float btmBtnTop = (btmBtnOffsets.y + btmBtnSizes.y)*vertRatio;
+		creatureBtnRect = new Rect(btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
+		inventoryBtnRect = new Rect(creatureBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
+		playersBtnRect = new Rect(inventoryBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
+		attacksBtnRect = new Rect(playersBtnRect.xMax + btmBtnOffsets.x*horizRatio, Screen.height - btmBtnTop, btmBtnSizes.x*horizRatio, btmBtnTop);
 	}
 	
 	void OnGUI() {
 		GUI.DrawTexture(topBarRect, topBarTexture);
+		GUI.DrawTexture(btmBarRect, btmBarTexture);
 		
 		if(GUI.Button (friendButtonRect, "", friendButtonStyle))
 		{
@@ -50,6 +71,22 @@ public class MainMenu : MonoBehaviour {
 		if(GUI.Button (messagesButtonRect, "", messagesButtonStyle))
 		{
 			Debug.Log ("messages button pressed");
+		}
+		if(GUI.Button (creatureBtnRect, "", creatureBtnStyle))
+		{
+			Debug.Log ("creature button pressed");
+		}
+		if(GUI.Button (inventoryBtnRect, "", inventoryBtnStyle))
+		{
+			Debug.Log ("inventory button pressed");
+		}
+		if(GUI.Button (playersBtnRect, "", playersBtnStyle))
+		{
+			Debug.Log ("players button pressed");
+		}
+		if(GUI.Button (attacksBtnRect, "", attacksBtnStyle))
+		{
+			Debug.Log ("attacks button pressed");
 		}
 	}
 	
