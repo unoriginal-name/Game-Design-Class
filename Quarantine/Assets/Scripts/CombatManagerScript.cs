aeff817d732932d3c8fe831bd73bc272f42cddbd
Devin Schwab <digidevin@gmail.com>
2013-10-10 09:18:13 +0800

    Created a timer class that broadcasts a timesup message. Contains a few methods for starting and stopping the timer.
    As a demo created a simple combat manager object/script that calls the timer for 10 seconds after a TimesUp message is received.
    
    Requires that a global world object be created so that other things can hear the TimesUp message

---
+++ Quarantine/Assets/Scripts/CombatManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatManagerScript : MonoBehaviour {
+
+    public CombatTimer combat_timer;
+
+	// Use this for initialization
+	void Start () {
+        combat_timer.StartTimer(10);
+	}
+
+    void TimesUp()
+    {
+        Debug.Log("Times Up");
+        combat_timer.StartTimer(10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Quarantine/Assets/Scripts/CombatTimer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatTimer : MonoBehaviour {
+
+    private double countdown_time;
+    private bool running = false;
+
+    public GameObject world;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    [RPC]
+    public bool StartTimer(double time)
+    {
+        Debug.Log("Starting timer for " + time + " seconds");
+        if (running)
+            return false;
+
+        countdown_time = time;
+        running = true;
+        return true;
+    }
+
+    [RPC]
+    public void StopTimer()
+    {
+        Debug.Log("Stopping timer");
+        running = false;
+    }
+
+    [RPC]
+    public double GetTimeLeft()
+    {
+        if (countdown_time <= 0)
+            return 0; // doesn't make sense to return a value less than 0
+
+        return countdown_time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!running)
+            return; // do nothing if not running
+
+        countdown_time -= Time.deltaTime; // subtract time since last update
+
+        if (countdown_time <= 0)
+        {
+            running = false;
+            Debug.Log("Time's up");
+            world.BroadcastMessage("TimesUp");
+        }
+	}
+}
