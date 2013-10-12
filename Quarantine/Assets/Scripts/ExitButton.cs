ced1d0cb79d024f284b5ff9c56bcfa6d1756634a
Devin Schwab <digidevin@gmail.com>
2013-10-12 21:45:00 +0800

    Added an exit button. Currently doesn't do anything.

---
+++ Quarantine/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour {
+	
+	public GUIStyle exit_btn_style;
+	
+	private ScreenRatio screen_ratio;
+	
+	public float left = 120;
+	public float top = 630;
+	public float width = 70;
+	public float height = 40;
+		
+	void Start () {
+		screen_ratio = (ScreenRatio)GameObject.Find ("Main Camera").GetComponent("ScreenRatio");
+	}
+	
+	void OnGUI() {
+		if(GUI.Button ( new Rect(left*screen_ratio.horiz, top*screen_ratio.vert, width*screen_ratio.horiz, height*screen_ratio.vert), "", exit_btn_style))
+		{
+			Debug.Log ("Exit button pressed");	
+		}
+	}
+}
---
+++ Quarantine/Assets/Scripts/ScreenRatio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRatio : MonoBehaviour {
+	
+	public int expected_width = 1280;
+	public int expected_height = 800;
+	
+	public float horiz = 1;
+	public float vert = 1;
+	
+	void Update()
+	{
+		horiz = (float)Screen.width/(float)expected_width;
+		vert = (float)Screen.height/(float)expected_height;
+	}
+}
