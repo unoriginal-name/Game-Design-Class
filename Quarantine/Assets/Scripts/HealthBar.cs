f22dfb2f5f75960aa15df80663198c33b7a3a93f
Devin Schwab <digidevin@gmail.com>
2013-10-12 22:19:49 +0800

    Added game objects for a bunch of the UI. However, not everything is placed. Health bars are placed but they currently are just static images.

---
+++ Quarantine/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	
+	public Texture health_bar_image;
+
+	private ScreenRatio screen_ratio;
+	
+	public float left = 120;
+	public float top = 470;
+	public float width = 400;
+	public float height = 400;
+		
+	void Start () {
+		screen_ratio = (ScreenRatio)GameObject.Find ("Main Camera").GetComponent("ScreenRatio");
+	}
+	
+	void OnGUI() {
+		GUI.DrawTexture(new Rect(left*screen_ratio.horiz, top*screen_ratio.vert, width*screen_ratio.horiz, height*screen_ratio.vert), health_bar_image);
+	}
+}
