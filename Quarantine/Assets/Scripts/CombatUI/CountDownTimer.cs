6ec4be81ceb489af86fd87ec0464ff7e1d01bc5c
Devin Schwab <digidevin@gmail.com>
2013-10-13 11:46:38 +0800

    Timer color changes based on how much time is left.

--- Quarantine/Assets/Scripts/CombatUI/CountDownTimer.cs
+++ Quarantine/Assets/Scripts/CombatUI/CountDownTimer.cs
@@ -19,9 +19,22 @@ public class CountDownTimer : MonoBehaviour {
 	
 	void OnGUI() {
 		float time_left = timer.GetTimeLeft();
+		
+		// change to yellow when less than 4 seconds left
+		// change to red when less than 2 seconds left
+		if(time_left < 2)
+			text_style.normal.textColor = new Color(1, 0, 0, 1);
+		else if(time_left < 4)
+			text_style.normal.textColor = new Color(1, 1, 0, 1);
+		else
+			text_style.normal.textColor = new Color(0, 1, 0, 1);
+		
 		if(time_left > 0)
 			GUI.Label (new Rect(left*screen_ratio.horiz, top*screen_ratio.vert, width*screen_ratio.horiz, height*screen_ratio.vert), String.Format("{0:F2}", time_left), text_style);
 		else
+		{
+			text_style.normal.textColor = new Color(1,1,1,1);
 			GUI.Label(new Rect(left*screen_ratio.horiz, top*screen_ratio.vert, width*screen_ratio.horiz, height*screen_ratio.vert), "Times Up!", text_style);
+		}
 	}
 }
