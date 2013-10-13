d171dd65c197e6bf34745a94419c315b9c081da8
Devin Schwab <digidevin@gmail.com>
2013-10-13 11:57:40 +0800

    Reorganized the control flow in the combat rules script

--- Quarantine/Assets/Scripts/CombatRules.cs
+++ Quarantine/Assets/Scripts/CombatRules.cs
@@ -7,8 +7,10 @@ public class CombatRules : MonoBehaviour {
 	
 	float WAIT_TIME = 1;
 	float wait_start = 0;
-	
 	bool waiting = false;
+		
+	private int player_move = -1;
+	private int enemy_move = -1;
 	
 	// Use this for initialization
 	void Start () {
@@ -16,13 +18,30 @@ public class CombatRules : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(!waiting)
-			return;
+	void Update () {	
+		if(player_move == -1 || enemy_move == -1)
+			return; // still waiting for at least one character to submit a move
+		else
+		{	
+			// start wait timer
+			if(!waiting)
+			{
+				waiting = true;
+				wait_start = Time.time;
+			}
+		}
 		
 		if(Time.time - wait_start > WAIT_TIME)
 		{
+			// stop waiting
 			waiting = false;
+			
+			// update the players
+			
+			// reset the moves
+			player_move = -1;
+			enemy_move = -1;
+			
 			timer.StartTimer(10.0f);
 		}
 	}
@@ -30,12 +49,17 @@ public class CombatRules : MonoBehaviour {
 	public void SubmitMove(string name, int move) {
 		// this is where health changes will be calculated
 		// and turn types will be calculated.
+		
+		if(name.Equals("Player"))
+			player_move = move;
+		else if(name.Equals ("Enemy"))
+			enemy_move = move;
+		else
+			Debug.LogError("Unknown player: " + name);		
 	}
 	
 	void TimesUp() {
-		waiting = true;
-		wait_start = Time.time;
-		
+
 	}
 	
 	void TimerPaused() {
