a2c3a9a763222e68701102bf79217014f5b21a19
Devin Schwab <digidevin@gmail.com>
2013-10-22 00:04:57 +0800

    Created metadata class SpriteAnimation (shows up in Unity inspector). This class is meant to hold all of the information about a sprite sheet animation. It will not actually do the animation though.
    Also deleted the metadata class since it isn't needed (all info is in the SpriteAnimation class instance)

---
+++ Quarantine/Assets/Scripts/SpriteLibrary/SpriteAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SpriteAnimation { //: MonoBehaviour {
+	
+	public Texture2D sprite_sheet; // texture to use for the sprites
+	public int rows = 0; // number of rows of sprites in specified sheet
+	public int cols = 0; // number of columns of sprites in specified sheet
+	public int num_frames = 0; // number of frames (this is only used if there are missing sprites in a row (e.g. 14 sprites in a 4x4 sprite sheet)
+	List<int> sequence; // defaults to in order, but if you need to play frames out of order then specify it here
+	public float default_fps = 24; // defaults to 24 frames, but changes this if needed. Animator will play at different speeds, but will default to this number
+
+	SpriteAnimation()
+	{
+		// if no num_frames specified then get it from
+		// the number of rows and columns
+		if(num_frames == 0)
+			num_frames = rows*cols;
+		
+		sequence = new List<int>(); // initialize the list so it can be used below
+		
+		// if no sequence specified then just play
+		// the indicies in order
+		if(sequence.Count == 0)
+		{
+			for(int i=0; i<num_frames; i++)
+			{
+				sequence.Add (i);	
+			}
+		}
+	}
+}
