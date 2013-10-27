7f6581ec7e1011a22826d52a4b233a25e2681db8
Rachid Lamouri <rxl244@case.edu>
2013-10-26 21:45:01 -0400

    Working on Gesture Demo 2

---
+++ Quarantine/Assets/Scripts/Gestures/GestureHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestureHandler : MonoBehaviour {
+	//private const float DOUBLE_TAP_TIME = .5f;
+	
+	public GameObject gestureObject;
+	
+	private GestureInterpreter gestureInterpreter;
+	
+	//private float oldTapTime;
+	private float newTapTime;
+	
+	void Start () {
+		gestureInterpreter = gestureObject.GetComponent<GestureInterpreter>();
+	}
+	
+	void Update () {
+		checkMove();
+	}
+	
+	private void checkMove(){
+		if(newTapTime != gestureInterpreter.LastTimeStamp && gestureInterpreter.LastGesture == GestureInterpreter.gesture.tap){
+			//oldTapTime = newTapTime;
+			newTapTime = gestureInterpreter.LastTimeStamp;
+			
+			
+			
+			//if(newTapTime - oldTapTime <= DOUBLE_TAP_TIME){				
+				Vector3 movePosition = gestureInterpreter.LastTapPosition;
+				movePosition.z = 0;
+				//print (movePosition.x + " " + movePosition.y);
+				this.gameObject.transform.rigidbody.MovePosition(movePosition);
+			//}
+		}
+	}
+}
