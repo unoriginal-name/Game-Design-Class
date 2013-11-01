3b37be86b6dfa8bd6ece8c7272b07f08e48b7a9b
Devin Schwab <digidevin@gmail.com>
2013-11-01 03:12:14 -0400

    Displays bacteria bubble that accelerates downwards. Currently doesn't do anything else.

---
+++ immunity/Assets/Scripts/Combat/BacteriaBubble.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BacteriaBubble : FSprite {
+	
+	private float _speedY;
+		
+	public BacteriaBubble() : base("punchyswarm_idle1") {
+		scale = 0.5f;
+		ListenForUpdate(HandleUpdate);
+	}
+	
+	public void HandleUpdate () {
+		_speedY -= 0.013f;
+		
+		this.y += _speedY;
+	}
+}
