c86003d11ebe713045e0d7d203459394783c135b
Devin Schwab <digidevin@gmail.com>
2013-11-01 06:09:40 -0400

    Main menu buttons are now working.

--- immunity/Assets/Scripts/TitlePage.cs
+++ immunity/Assets/Scripts/TitlePage.cs
@@ -23,10 +23,9 @@ public class TitlePage : ImmunityPage
 		background_.x = background_.width/2.0f - Futile.screen.halfWidth;
 		background_.y = background_.height/2.0f - Futile.screen.halfHeight;
 		
-		playButton_ = new FButton("BigButton", "BigButton"); // "BigButton", "ButtonClickSound");
+		playButton_ = new FButton("BigButton", "BigButton_down"); // "BigButton_down"); //"ButtonClickSound");
 		playButton_.AddLabel("ImmunityFont", "Play", Color.white);
 		AddChild(playButton_);
-		
 		
 		playButton_.SignalRelease += HandlePlayButtonRelease;
 		
@@ -35,7 +34,7 @@ public class TitlePage : ImmunityPage
 		Go.to(playButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleX", 1.0f).setEaseType(EaseType.BackOut));
 		Go.to(playButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleY", 0.5f).setEaseType(EaseType.BackOut));
 		
-		howToPlayButton_ = new FButton("BigButton", "BigButton"); // "BigButton", "ButtonClickSound");
+		howToPlayButton_ = new FButton("BigButton", "BigButton_down"); // "BigButton", "ButtonClickSound");
 		howToPlayButton_.AddLabel("ImmunityFont", "How To Play", Color.white);
 		AddChild(howToPlayButton_);
 		
@@ -43,11 +42,11 @@ public class TitlePage : ImmunityPage
 		
 		howToPlayButton_.scale = 0.0f;
 		
-		Go.to(howToPlayButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleX", 1.0f).setEaseType(EaseType.BackOut));
-		Go.to(howToPlayButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleY", 0.5f).setEaseType(EaseType.BackOut));
+		// Go.to(howToPlayButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleX", 1.0f).setEaseType(EaseType.BackOut));
+		//Go.to(howToPlayButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleY", 0.5f).setEaseType(EaseType.BackOut));
 
 		
-		settingsButton_ = new FButton("BigButton", "BigButton"); // "BigButton", "ButtonClickSound");
+		settingsButton_ = new FButton("BigButton", "BigButton_down"); // "BigButton", "ButtonClickSound");
 		settingsButton_.AddLabel("ImmunityFont", "Settings", Color.white);
 		AddChild(settingsButton_);
 		
@@ -60,8 +59,10 @@ public class TitlePage : ImmunityPage
 		Go.to(settingsButton_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scaleY", 0.5f).setEaseType(EaseType.BackOut));
 		
 		
-		playButton_.y = (howToPlayButton_.hitRect.yMax - howToPlayButton_.hitRect.yMin)/2.0f + 50.0f ;
-		settingsButton_.y = -((howToPlayButton_.hitRect.yMax - howToPlayButton_.hitRect.yMin)/2.0f + 50.0f);
+		//playButton_.y = (howToPlayButton_.hitRect.yMax - howToPlayButton_.hitRect.yMin)/2.0f + 50.0f ;
+		//settingsButton_.y = -((howToPlayButton_.hitRect.yMax - howToPlayButton_.hitRect.yMin)/2.0f + 50.0f);
+		playButton_.y = 50.0f;
+		settingsButton_.y = -50.0f;
 	}
 	
 	private void HandlePlayButtonRelease(FButton button)
