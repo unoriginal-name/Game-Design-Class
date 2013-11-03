9f3904e992cbef14f08ca2e07015e122b5ea9db0
Devin Schwab <digidevin@gmail.com>
2013-11-03 16:18:05 -0500

    Added a Speed attribute to the player's character.

--- immunity/Assets/Scripts/Combat/PlayerCharacter.cs
+++ immunity/Assets/Scripts/Combat/PlayerCharacter.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class PlayerCharacter : FSprite {
 	
+	private float speed_;
+	
 	public PlayerCharacter() : base("hhhh")
 	{
 		ListenForUpdate(HandleUpdate);
 		
 		scale = 0.15f;
 		
+		speed_ = 1.0f;
+		
 		y = -Futile.screen.halfHeight + height/2.0f + 50.0f;
 		x = -Futile.screen.halfWidth + width/2.0f + 150.0f;
+	}
+	
+	public float Speed
+	{
+		get {return speed_;}
+		set {speed_ = value; }
 	}
 	
 	void HandleUpdate () {
