3de82d5062c140bb8937b19445528d6317a10ddc
Devin Schwab <digidevin@gmail.com>
2013-11-19 21:16:52 +0800

    Reenabled the enemy's random behaviors.

--- immunity/Assets/Scripts/Combat/EnemyCharacter.cs
+++ immunity/Assets/Scripts/Combat/EnemyCharacter.cs
@@ -86,7 +86,7 @@ public class EnemyCharacter : FAnimatedSprite {
 		{
 			float behavior_selection = Random.value;
 			
-			/*if(behavior_selection < .1f)
+			if(behavior_selection < .1f)
 			{
 				// switch to move_towards_player behavior
 				curr_behavior_ = BehaviorType.MOVE_TOWARDS_PLAYER;
@@ -110,13 +110,13 @@ public class EnemyCharacter : FAnimatedSprite {
 				curr_behavior_ = BehaviorType.BLOCK;
 				Debug.Log("Behavior: Block");
 			}
-			else if(behavior_selection < .5f)
+			else if(behavior_selection < .45f)
 			{
 				// switch to swarm behavior	
 				curr_behavior_ = BehaviorType.SPAWN_SWARM;
 				spawn_count = 0;
 				Debug.Log("Behavior: spawn swarm");
-			}*/
+			}
 			behavior_start_time_ = Time.time;
 		}
 	}
