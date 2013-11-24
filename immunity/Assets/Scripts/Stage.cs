fd88be19a949d5a5c1e6cf65c232df389f25a9ee
Pat Melvin <stpatrick923@gmail.com>
2013-11-24 00:10:02 -0500

    Animations
    Animations work for stomach

--- immunity/Assets/Scripts/Stage.cs
+++ immunity/Assets/Scripts/Stage.cs
@@ -10,9 +10,12 @@ public class Stage : FStage {
 	FSprite level_mid_sprite;
 	FSprite level_foremid_sprite;
 	FSprite level_foreground_sprite;
-	FSprite level_animation_sprite;
-	FSprite level_animation_sprite2;
-	FSprite level_animation_sprite3;
+
+	FAnimatedSprite level_animations;
+
+	FAnimation level_animation_sprite;
+	FAnimation level_animation_sprite2;
+	FAnimation level_animation_sprite3;
 	
 	private FParallaxContainer background;
 	private FParallaxContainer midBack;
@@ -94,9 +97,6 @@ public class Stage : FStage {
 		mid.camObject = ImmunityCombatManager.instance.camera_;
 		foreMid.camObject = ImmunityCombatManager.instance.camera_;
 		foreground.camObject = ImmunityCombatManager.instance.camera_;
-		animation.camObject = ImmunityCombatManager.instance.camera_;
-		animation2.camObject = ImmunityCombatManager.instance.camera_;
-		animation3.camObject = ImmunityCombatManager.instance.camera_;
 	}
 
 	private void initializeContainers()
@@ -107,9 +107,9 @@ public class Stage : FStage {
 		mid = new FParallaxContainer();
 		foreMid = new FParallaxContainer();
 		foreground = new FParallaxContainer();
-		animation = new FParallaxContainer();
-		animation2 = new FParallaxContainer();
-		animation3 = new FParallaxContainer();
+
+		level_animations = new FAnimatedSprite("Bubble");
+
 		Debug.Log("Finished calling initialize containers");
 	}
 
@@ -120,10 +120,9 @@ public class Stage : FStage {
 			level_background_sprite = new FSprite("Brain_Background");
 			level_mid_sprite = new FSprite("Brain_Mid");
 			level_foreground_sprite = new FSprite("Brain_Fore");
-			level_animation_sprite = new FSprite("neuron_fast_60_animation");
-			level_animation_sprite2 = new FSprite("neuron_fast_80_animation");
-			level_animation_sprite3 = new FSprite("neuron_fast_40_animation");
-
+			//level_animation_sprite = new FAnimtion"neuron_fast_60_animation");
+			//level_animation_sprite2 = new FSprite("neuron_fast_80_animation");
+			//level_animation_sprite3 = new FSprite("neuron_fast_40_animation");
 
 			background.size = new Vector2(2000, 1000); //(x, y) such that the vector is between the world bounds (background size) and camera bounds (1024x768)
 			mid.size = new Vector2 (1024, 768); //(x, y) such that the vector is smaller than the camera (1024x768)
@@ -137,7 +136,7 @@ public class Stage : FStage {
 			level_mid_sprite = new FSprite("LungsMidBack");
 			level_foremid_sprite = new FSprite("LungsForeMid");
 			level_foreground_sprite = new FSprite("LungsFore");
-			level_animation_sprite = new FSprite("lung_dust");
+			//level_animation_sprite = new FSprite("lung_dust");
 
 			background.size = new Vector2 (2000, 1000); //(x, y) such that the vector is between the world bounds (background size) and camera bounds (1024x768)
 			midBack.size = new Vector2 (1024, 768);
@@ -153,12 +152,19 @@ public class Stage : FStage {
 			level_background_sprite = new FSprite("Stomach_Lake");
 			level_mid_sprite = new FSprite("Stomach_Mid");
 			level_foreground_sprite = new FSprite("Stomach_Fore");
-			level_animation_sprite = new FSprite("stomach_animations");
+
+			int[] bubble_frames = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+			int[] ripple_frames = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+			FAnimation bubbles = new FAnimation("Bubble", bubble_frames, 100, true);
+			FAnimation ripples = new FAnimation("Ripple", ripple_frames, 100, true);
+
+			level_animations.addAnimation(bubbles);
+			level_animations.addAnimation(ripples);
 
 			background.size = new Vector2  (2000, 1000);
 			mid.size = new Vector2  (1024, 768);
 			foreground.size = new Vector2  (4096, 2048);
-			animation.size = new Vector2 (2000, 1000);
+			//animation.size = new Vector2 (2000, 1000);
 		}
 	}
 
@@ -168,18 +174,23 @@ public class Stage : FStage {
 		mid.AddChild (level_mid_sprite);
 		foreground.AddChild (level_foreground_sprite);
 
+
 		//check for things that not every stage has.  If they're present, add them.
 		//Yes, I know McCabe complexity is now 5.  Eat me.
 		if (level_midback_sprite != null)
 			midBack.AddChild(level_midback_sprite);
 		if (level_foremid_sprite != null)
 			foreMid.AddChild(level_foremid_sprite);
+
+		//these all end up getting added to level_animations
+		/*
 		if (level_animation_sprite != null)
 			animation.AddChild(level_animation_sprite);
 		if (level_animation_sprite2 != null)
 			animation2.AddChild (level_animation_sprite2);
 		if (level_animation_sprite3 != null)
 			animation3.AddChild (level_animation_sprite3);
+			*/
 	}
 
 	public FParallaxContainer getBackground()
@@ -207,9 +218,9 @@ public class Stage : FStage {
 		return foreground;
 	}
 
-	public FParallaxContainer getAnimation()
+	public FAnimatedSprite getAnimation()
 	{
-		return animation;
+		return level_animations;
 	}
 
 	public FParallaxContainer getAnimation2()
