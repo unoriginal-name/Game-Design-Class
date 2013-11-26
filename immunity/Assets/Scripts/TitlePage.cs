d4ec2e0e72f24ed5e92942570f75ab614eecbf11
Rachid Lamouri <rxl244@case.edu>
2013-11-25 23:17:44 -0500

    Finished main menus (credits, main and level select)

--- immunity/Assets/Scripts/TitlePage.cs
+++ immunity/Assets/Scripts/TitlePage.cs
@@ -7,9 +7,9 @@ public class TitlePage : ImmunityPage, FMultiTouchableInterface
 	private ImmunityMenu immunityMenu;
 
 	private FSprite background_;
-	private Rect play_button_rect_;
-	private Rect settings_button_rect_;
-	
+	private FButton play_button;
+	private FButton credits_button;
+
 	public TitlePage()
 	{
 		EnableMultiTouch();
@@ -18,67 +18,60 @@ public class TitlePage : ImmunityPage, FMultiTouchableInterface
 	
 	override public void Start()
 	{
+		// Access immunity menu
 		immunityMenu = GameObject.Find ("Futile").GetComponent<ImmunityMenu>();
 
+		// Sound
 		FSoundManager.StopMusic();
 		FSoundManager.UnloadAllSoundsAndMusic();
 		FSoundManager.PlayMusic("background_music");
 		FSoundManager.PreloadSound("button_click");
 	
-		background_ = new FSprite("start_screen");
+		// GUI objects
+		background_ = new FSprite("start screen final");
+		play_button = new FButton("Play","PlayPressed");
+		credits_button = new FButton("Credits", "CreditsPressed");
+
+		// Add objects to stage
 		AddChild(background_);
-		
+		AddChild(play_button);
+		AddChild(credits_button);
+
+		// Scale and set position
 		background_.scale = 0.0f;
+
+		play_button.scale = .8f;
+		play_button.x = 180;
+		play_button.y = -50;
 		
+		credits_button.x = 200;
+		credits_button.y = -280;
+
+		// Set button delegates
+		play_button.SignalRelease += HandlePlayButton;
+		credits_button.SignalRelease += HandleCreditsButton;
+
 		Go.to(background_, 0.5f, new TweenConfig().setDelay(0.1f).floatProp("scale", 1.0f).setEaseType(EaseType.BackOut));
-		
-		play_button_rect_ = new Rect(Futile.screen.halfWidth*.15f, Futile.screen.halfHeight*-0.05f, Futile.screen.halfWidth*.35f, Futile.screen.halfHeight*.3f);
-		settings_button_rect_ = new Rect(Futile.screen.halfWidth*.15f, Futile.screen.halfHeight*-0.45f, Futile.screen.halfWidth*.6f, Futile.screen.halfHeight*.3f);
 	}
 	
-	private void HandlePlayButtonRelease(FButton button)
-	{
-		playButtonClicked();
+	private void HandlePlayButton(FButton button){
+		Debug.Log("Play Button Clicked");
+		immunityMenu.GoToMenu(PageType.LevelSelectPage);
 	}
 	
-	private void HandleHowToPlayButtonRelease(FButton button)
+	private void HandleCreditsButton(FButton button)
 	{
-		Debug.Log("How To Play Button Clicked");
+		Debug.Log("Credits Button Clicked");
+		immunityMenu.GoToMenu(PageType.CreditsPage);
 	}
-	
-	private void HandleSettingsButtonRelease(FButton button)
-	{
-		Debug.Log("Settings Button Clicked");
-	}
-	
-	protected void HandleUpdate()
-	{
-	}
-	
-	public void HandleMultiTouch(FTouch[] touches)
-	{
-		
-		foreach(FTouch touch in touches)
-		{	
-			if(touch.phase == TouchPhase.Ended)
-			{
-				if(play_button_rect_.xMin <= touch.position.x && play_button_rect_.xMax >= touch.position.x &&
-				play_button_rect_.yMin <= touch.position.y && play_button_rect_.yMax >= touch.position.y)
-				{
-					playButtonClicked();
-				}
-				else if(settings_button_rect_.xMin <= touch.position.x && settings_button_rect_.xMax >= touch.position.x &&
-				settings_button_rect_.yMin <= touch.position.y && settings_button_rect_.yMax >= touch.position.y)
-				{
-					Debug.Log("Settings button clicked");	
-				}
+
+	public void HandleMultiTouch(FTouch[] touches){
+		foreach(FTouch touch in touches){	
+			if(touch.phase == TouchPhase.Ended){
 			}			
 		}
 	}
 
-	public void playButtonClicked(){
-		Debug.Log("Play Button Clicked");
-		//Application.LoadLevel("BubblePopDemo");
-		immunityMenu.GoToMenu(PageType.LevelSelectPage);
+	protected void HandleUpdate(){
 	}
 }
